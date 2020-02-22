94f68f5a06f91f155ac3eef1beb54e7b49d3a8d8
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2020-02-23 08:21:15 +0900

    add package

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='openjtalk-label-getter',
     version='0.0.1',
+    packages=find_namespace_packages(),
     url='https://github.com/Hiroshiba/openjtalk-label-getter',
     author='Kazuyuki Hiroshiba',
     author_email='hihokaruta@gmail.com',
