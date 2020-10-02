58e5398ea3611a18a728ec56e8e00cdcb4feac75
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2020-10-02 03:04:59 +0000

    add full context label

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # OpenJTalk label getter
 OpenJTalkで音声合成する際に得られるラベルを取得します。
+音素ラベルとフルコンテキストラベルに対応しています。
 
 ## Requirements
 ```bash
@@ -13,7 +14,9 @@ pip install git+https://github.com/Hiroshiba/openjtalk-label-getter
 
 ## Usage
 ```bash
-$ openjtalk_label_getter こんにちは
+# 音素ラベル
+openjtalk_label_getter こんにちは --output_type phoneme
+"""
 0.0000 0.3100 sil
 0.3100 0.4350 k
 0.4350 0.5150 o
@@ -25,6 +28,23 @@ $ openjtalk_label_getter こんにちは
 0.8300 0.9650 w
 0.9650 1.0800 a
 1.0800 1.6050 sil
+"""
+
+# フルコンテキストラベル
+python openjtalk_label_getter/__init__.py こんにちは --output_type full_context_label
+"""
+0.0000 0.3100 xx^xx-sil+k=o/A:xx+xx+xx/B:xx-xx_xx/C:xx_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:xx_xx#xx_xx@xx_xx|xx_xx/G:5_5%0_xx-xx/H:xx_xx/I:xx-xx@xx+xx&xx-xx|xx+xx/J:1_5/K:1+1-5
+0.3100 0.4350 xx^sil-k+o=N/A:-4+1+5/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+0.4350 0.5150 sil^k-o+N=n/A:-4+1+5/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+0.5150 0.5750 k^o-N+n=i/A:-3+2+4/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+0.5750 0.6350 o^N-n+i=ch/A:-2+3+3/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+0.6350 0.6950 N^n-i+ch=i/A:-2+3+3/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+0.6950 0.7850 n^i-ch+i=w/A:-1+4+2/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+0.7850 0.8300 i^ch-i+w=a/A:-1+4+2/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+0.8300 0.9650 ch^i-w+a=sil/A:0+5+1/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+0.9650 1.0800 i^w-a+sil=xx/A:0+5+1/B:xx-xx_xx/C:09_xx+xx/D:xx+xx_xx/E:xx_xx!xx_xx-xx/F:5_5#0_xx@1_1|1_5/G:xx_xx%xx_xx-xx/H:xx_xx/I:1-5@1+1&1-1|1+5/J:xx_xx/K:1+1-5
+1.0800 1.6050 w^a-sil+xx=xx/A:xx+xx+xx/B:xx-xx_xx/C:xx_xx+xx/D:xx+xx_xx/E:5_5!0_xx-xx/F:xx_xx#xx_xx@xx_xx|xx_xx/G:xx_xx%xx_xx-xx/H:1_5/I:xx-xx@xx+xx&xx-xx|xx+xx/J:xx_xx/K:1+1-5
+"""
 ```
 
 ## LICENSE
--- openjtalk_label_getter/__init__.py
+++ openjtalk_label_getter/__init__.py
@@ -1,71 +1,123 @@
 import argparse
 import re
+import shutil
 from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
 from subprocess import PIPE, Popen
+from tempfile import TemporaryDirectory
+from typing import Optional
+
+
+class OutputType(str, Enum):
+    phoneme = "phoneme"
+    full_context_label = "full_context_label"
 
 
 @dataclass
-class Phoneme:
+class Label:
     start: float
     end: float
-    phoneme: str
+    label: str
 
 
 def openjtalk_label_getter(
-        text: str,
-        openjtalk_command: str,
-        dict_path: Path,
-        htsvoice_path: Path,
-        output_wave_path: Path,
-        output_log_path: Path,
+    text: str,
+    openjtalk_command: str,
+    dict_path: Path,
+    htsvoice_path: Path,
+    output_wave_path: Optional[Path],
+    output_log_path: Optional[Path],
+    output_type: OutputType,
 ):
-    p_text = Popen(['echo', text], stdout=PIPE)
-    p_openjtalk = Popen(
-        [
-            openjtalk_command,
-            '-x', dict_path,
-            '-m', htsvoice_path,
-            '-ow', output_wave_path,
-            '-ot', output_log_path,
-        ],
-        stdin=p_text.stdout,
-    )
-    p_openjtalk.wait()
+    p_text = Popen(["echo", text], stdout=PIPE)
+    with TemporaryDirectory() as d:
+        tmp_dir = Path(d)
 
-    log = output_log_path.read_text()
-    labels = re.split(r'\[.+\]', log)[2].splitlines()
+        tmp_output_wave_path = tmp_dir.joinpath("output.wav")
+        tmp_output_log_path = tmp_dir.joinpath("output.txt")
+        p_openjtalk = Popen(
+            [
+                openjtalk_command,
+                "-x",
+                dict_path,
+                "-m",
+                htsvoice_path,
+                "-ow",
+                tmp_output_wave_path,
+                "-ot",
+                tmp_output_log_path,
+            ],
+            stdin=p_text.stdout,
+        )
+        p_openjtalk.wait()
 
-    phonemes = []
+        log = tmp_output_log_path.read_text()
+
+        if output_wave_path is not None:
+            shutil.move(str(tmp_output_wave_path), str(output_wave_path))
+        if output_log_path is not None:
+            shutil.move(str(tmp_output_log_path), str(output_log_path))
+
+    labels = re.split(r"\[.+\]", log)[2].splitlines()
+
+    outputs = []
     for label in filter(lambda x: len(x) > 0, labels):
-        match = re.fullmatch(r'(\d+) (\d+) .+\^.+-(.+)\+.+=.+/A.+/B.+/C.+/D.+/E.+/F.+/G.+/H.+/I.+/J.+/K.+', label)
-        start, end, phoneme = match.groups()
-        phonemes.append(Phoneme(
-            start=int(start) / 10 / 1000 / 1000,
-            end=int(end) / 10 / 1000 / 1000,
-            phoneme=phoneme,
-        ))
-    return phonemes
+        match = re.fullmatch(
+            r"(\d+) (\d+) (.+\^.+-(.+)\+.+=.+/A.+/B.+/C.+/D.+/E.+/F.+/G.+/H.+/I.+/J.+/K.+)",
+            label,
+        )
+        assert match is not None
+
+        start, end, full_context_label, phoneme = match.groups()
+
+        outputs.append(
+            Label(
+                start=int(start) / 10 / 1000 / 1000,
+                end=int(end) / 10 / 1000 / 1000,
+                label=phoneme
+                if output_type == OutputType.phoneme
+                else full_context_label,
+            )
+        )
+    return outputs
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('text')
-    parser.add_argument('--openjtalk_command',
-                        default='open_jtalk')
-    parser.add_argument('--dict_path', type=Path,
-                        default=Path('/var/lib/mecab/dic/open-jtalk/naist-jdic'))
-    parser.add_argument('--htsvoice_path', type=Path,
-                        default=Path('/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice'))
-    parser.add_argument('--output_wave_path', type=Path,
-                        default=Path('/tmp/tmp_openjtalk_label_getter.wav'))
-    parser.add_argument('--output_log_path', type=Path,
-                        default=Path('/tmp/tmp_openjtalk_label_getter.txt'))
-    phonemes = openjtalk_label_getter(**vars(parser.parse_args()))
+    parser.add_argument("text")
+    parser.add_argument("--openjtalk_command", default="open_jtalk")
+    parser.add_argument(
+        "--dict_path",
+        type=Path,
+        default=Path("/var/lib/mecab/dic/open-jtalk/naist-jdic"),
+    )
+    parser.add_argument(
+        "--htsvoice_path",
+        type=Path,
+        default=Path(
+            "/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice"
+        ),
+    )
+    parser.add_argument(
+        "--output_wave_path",
+        type=Path,
+    )
+    parser.add_argument(
+        "--output_log_path",
+        type=Path,
+    )
+    parser.add_argument(
+        "--output_type",
+        type=OutputType,
+        default=OutputType.phoneme,
+        choices=[t.value for t in OutputType],
+    )
+    outputs = openjtalk_label_getter(**vars(parser.parse_args()))
 
-    for p in phonemes:
-        print(f'{p.start:.4f} {p.end:.4f} {p.phoneme}')
+    for p in outputs:
+        print(f"{p.start:.4f} {p.end:.4f} {p.label}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
