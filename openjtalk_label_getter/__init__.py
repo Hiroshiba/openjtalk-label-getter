3737eec59ca5d35a5a43f31d1f6c51c2835d9030
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2021-03-15 03:55:43 +0000

    add yomi

--- openjtalk_label_getter/__init__.py
+++ openjtalk_label_getter/__init__.py
@@ -7,18 +7,19 @@ from enum import Enum
 from pathlib import Path
 from subprocess import PIPE, Popen
 from tempfile import TemporaryDirectory
-from typing import Optional
+from typing import List, Optional
 
 
 class OutputType(str, Enum):
     phoneme = "phoneme"
     full_context_label = "full_context_label"
+    yomi = "yomi"
 
 
 @dataclass
 class Label:
-    start: float
-    end: float
+    start: Optional[float]
+    end: Optional[float]
     label: str
 
 
@@ -30,6 +31,7 @@ def openjtalk_label_getter(
     output_wave_path: Optional[Path],
     output_log_path: Optional[Path],
     output_type: OutputType,
+    without_span: bool,
 ):
     p_text = Popen(["echo", text], stdout=PIPE)
     with TemporaryDirectory() as d:
@@ -59,27 +61,51 @@ def openjtalk_label_getter(
         if output_log_path is not None:
             shutil.move(str(tmp_output_log_path), str(output_log_path))
 
-    labels = re.split(r"\[.+\]", log)[2].splitlines()
+    outputs: List[Label] = []
 
-    outputs = []
-    for label in filter(lambda x: len(x) > 0, labels):
-        match = re.fullmatch(
-            r"(\d+) (\d+) (.+\^.+-(.+)\+.+=.+/A.+/B.+/C.+/D.+/E.+/F.+/G.+/H.+/I.+/J.+/K.+)",
-            label,
-        )
-        assert match is not None
-
-        start, end, full_context_label, phoneme = match.groups()
-
-        outputs.append(
-            Label(
-                start=int(start) / 10 / 1000 / 1000,
-                end=int(end) / 10 / 1000 / 1000,
-                label=phoneme
-                if output_type == OutputType.phoneme
-                else full_context_label,
+    if output_type in [OutputType.phoneme, OutputType.full_context_label]:
+        labels = re.split(r"\[.+\]", log)[2].splitlines()
+        for label in filter(lambda x: len(x) > 0, labels):
+            match = re.fullmatch(
+                r"(\d+) (\d+) (.+\^.+-(.+)\+.+=.+/A.+/B.+/C.+/D.+/E.+/F.+/G.+/H.+/I.+/J.+/K.+)",
+                label,
             )
-        )
+            assert match is not None
+
+            start, end, full_context_label, phoneme = match.groups()
+
+            outputs.append(
+                Label(
+                    start=int(start) / 10 / 1000 / 1000 if not without_span else None,
+                    end=int(end) / 10 / 1000 / 1000 if not without_span else None,
+                    label=(
+                        phoneme
+                        if output_type == OutputType.phoneme
+                        else full_context_label
+                    ),
+                )
+            )
+
+    elif output_type == OutputType.yomi:
+        assert without_span
+
+        labels = re.split(r"\[.+\]", log)[1].splitlines()
+
+        for label in filter(lambda x: len(x) > 0, labels):
+            match = re.fullmatch(
+                r".+,.+,.+,.+,.+,.+,.+,.+,.+,(.+),.+,.+,.+",
+                label,
+            )
+            assert match is not None
+
+            (yomi,) = match.groups()
+
+            for s in yomi:
+                outputs.append(Label(start=None, end=None, label=s))
+
+    else:
+        raise ValueError(output_type)
+
     return outputs
 
 
@@ -99,24 +125,22 @@ def main():
             "/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice"
         ),
     )
-    parser.add_argument(
-        "--output_wave_path",
-        type=Path,
-    )
-    parser.add_argument(
-        "--output_log_path",
-        type=Path,
-    )
+    parser.add_argument("--output_wave_path", type=Path)
+    parser.add_argument("--output_log_path", type=Path)
     parser.add_argument(
         "--output_type",
         type=OutputType,
         default=OutputType.phoneme,
         choices=[t.value for t in OutputType],
     )
+    parser.add_argument("--without_span", action="store_true")
     outputs = openjtalk_label_getter(**vars(parser.parse_args()))
 
     for p in outputs:
-        print(f"{p.start:.4f} {p.end:.4f} {p.label}")
+        if p.start is not None:
+            print(f"{p.start:.4f} {p.end:.4f} {p.label}")
+        else:
+            print(f"{p.label}")
 
 
 if __name__ == "__main__":
